757180c255151567ac906881d16d6f1e9e8de807
Tran Van Si <sitranphamdong@gmail.com>
2023-11-01 10:37:36 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,9 @@ return a-b;}
 int tich(int a, int b) {
 	return a*b;
 }
-float thuong(int a, int b);
+float thuong(int a, int b) {
+	return a/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
