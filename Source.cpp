519544dbd3f002d65a2d6b922a30ad5d8aa91b83
Tran Van Si <sitranphamdong@gmail.com>
2023-11-01 10:30:06 +0700

    tich

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b) {
+	return a*b;
+}
 float thuong(int a, int b);
 int main()
 {
